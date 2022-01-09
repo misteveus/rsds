86bbf63072449b995756b108240210560ee26f4f
Steven Parker <steven.parker@oit.edu>
2022-01-09 00:07:30 -0800

    move to rust edition2021 and add more unit tests for Stack

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ pub mod stack {
     use std::fmt;
     use std::vec::Vec;
 
+    /// Error type indicating the stack is full. 
     #[derive(Debug, PartialEq)]
     pub struct StackFullError;
 
@@ -51,7 +52,7 @@ pub mod stack {
 
 #[cfg(test)]
 mod test_stack {
-    use crate::rsds::stack;
+    use crate::stack;
 
     #[test]
     fn stack_push() {
@@ -65,7 +66,7 @@ mod test_stack {
     }
 
     #[test]
-    fn stack_push_when_full() {
+    fn stack_push_when_full_returns_stackfullerror() {
         let mut stack = stack::Stack::new(5);
 
         let mut ret = stack.push(542u32);
@@ -96,5 +97,27 @@ mod test_stack {
 
         let popped_val = stack.pop();
         assert_eq!(popped_val, Some(542u32));
+    }
+
+    #[test]
+    fn stack_pop_when_empty_retuns_none() {
+        let mut stack = stack::Stack::<u32>::new(5);
+
+        let ret = stack.pop();
+        assert_eq!(ret, None);
+    }
+
+    #[test]
+    fn stack_new_returns_empty_stack() {
+        let stack = stack::Stack::<u32>::new(5);
+
+        assert_eq!(stack.stack.len(), 0);
+    }
+
+    #[test]
+    fn stack_new_returns_stack_with_correct_capacity() {
+        let stack = stack::Stack::<u32>::new(5);
+
+        assert_eq!(stack.stack.capacity(), 5);
     }
 }
